a1512010d7434558d49843ccad8330986979caae
Leonora <leonoraver77@hotmail.com>
2025-11-24 03:46:24 +0100

    modal window styles and functionality

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -68,38 +68,37 @@ async function displayCategories(){
     });
 }
 
+function AdminMode() {
+    const token = localStorage.getItem('token');
 
+    if (token) {
+        document.getElementById('admin-bar').style.display = 'flex';
+
+        const loginLink = document.querySelector('.login-link');
+        if (loginLink) {
+            loginLink.textContent = 'logout';
+            loginLink.addEventListener('click', (e) => {
+                e.preventDefault();
+                localStorage.removeItem('token');
+                window.location.reload();
+            });
+        }
+
+        const categoriesContainer = document.querySelector('.categories');
+        if (categoriesContainer) categoriesContainer.style.display = 'none';
+
+        document.querySelectorAll('.js-edit-mode').forEach(btn => {
+            btn.style.display = 'inline';
+        });
+    }
+}
 
 
 fetchGalery();
 
 fetchcategories();
+AdminMode();
     
 // Vérifie si un token est présent => utilisateur connecté
-const token = localStorage.getItem('token');
 
-if (token) {
-    // Affiche le bandeau admin
-    const adminBar = document.getElementById('admin-bar');
-    if (adminBar) adminBar.style.display = 'flex';
-
-    // Change "login" en "logout"
-    const loginLink = document.querySelector('.login-link');
-    if (loginLink) {
-        loginLink.textContent = 'logout';
-        loginLink.addEventListener('click', (e) => {
-            e.preventDefault();
-            localStorage.removeItem('token');
-            window.location.reload();
-        });
-    }
-
-    // Masquer les filtres
-    const categoriesContainer = document.querySelector('.categories');
-    if (categoriesContainer) categoriesContainer.style.display = 'none';
-
-    // Affiche les boutons de modification
-    const editButtons = document.querySelectorAll('.js-edit-mode');
-    editButtons.forEach(btn => btn.style.display = 'inline');
-}
 
